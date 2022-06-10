7efebd5136ccd17721504f83e1d994d76073537c
R3ALF4KT <aria.leer@outlook.com>
2022-06-10 13:27:24 +0200

    Update

--- GigChat/src/application/Controller.java
+++ GigChat/src/application/Controller.java
@@ -1,14 +1,12 @@
 package application;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -57,6 +55,35 @@ public class Controller {
 		
 		chatBox.setLayoutY( currentScene.getHeight() - chatBox.getHeight() );
 		chatBox.setPrefWidth( currentScene.getWidth() - righttab.getWidth() - lefttab.getWidth() );
+		
+		ScrollPane ScrollPaneMSG = (ScrollPane) currentScene.lookup("#Scrl");
+		
+		ScrollPaneMSG.setPrefHeight(currentScene.getHeight() - tabbar.getHeight() - chatBox.getHeight() );
+		ScrollPaneMSG.setPrefWidth( currentScene.getWidth() - righttab.getWidth() - lefttab.getWidth() );
+		
+		Pane MessagesBox = (Pane) currentScene.lookup("#MessagesBox");
+		
+		MessagesBox.setPrefHeight(currentScene.getHeight() - tabbar.getHeight() - chatBox.getHeight());
+		MessagesBox.setPrefWidth( currentScene.getWidth() - righttab.getWidth() - lefttab.getWidth()  - righttab.getWidth() / 10.5);
+		
+		for(int i = 0; i < MessagesBox.getChildren().size(); i++) {
+			
+			((Pane) MessagesBox.getChildren().get(i)).setPrefWidth( currentScene.getWidth() - righttab.getWidth() - lefttab.getWidth() );
+			
+		}
+		
+		MessagesBox.setPrefHeight( ((Pane)MessagesBox.getChildren().get(MessagesBox.getChildren().size()-1)).getLayoutY() + ((Pane)MessagesBox.getChildren().get(MessagesBox.getChildren().size()-1)).getPrefHeight());
+
+		for(int i = 0; i < MessagesBox.getChildren().size(); i++) {
+			
+			Pane messageContents = (Pane) MessagesBox.getChildren().get(i);
+			
+			TextFlow textflow = (TextFlow) messageContents.getChildren().get(0);
+			
+			textflow.setPrefWidth( MessagesBox.getPrefWidth() );
+			
+			
+		}
 		
 	}
 
--- GigChat/src/application/Main.java
+++ GigChat/src/application/Main.java
@@ -1,28 +1,22 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+	
+	Scene scene = null;
 	
 	public void start(Stage primaryStage) {
 		try {
@@ -33,7 +27,7 @@ public class Main extends Application {
 
 	        Parent root = FXMLLoader.load( fxmlfile );
 	        
-	        Scene scene = new Scene(root);
+	        scene = new Scene(root);
 	        
 	        primaryStage.setTitle("Guessing Game");
 	        
@@ -42,9 +36,69 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 
 			primaryStage.show();
+
+	        showMessage("WWWWWWWWWWWWWWWWWWWWWWWWWWWW".repeat(10));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void showMessage( String content ) {
+		
+		Pane MessagesBox = (Pane) scene.lookup("#MessagesBox");
+		
+		Pane messageContents = new Pane();
+		
+		messageContents.setPrefHeight(scene.getHeight() / 20);
+		messageContents.setPrefWidth(scene.getWidth() - ((Pane) (scene.lookup("#lefttab"))).getWidth() - ((Pane) (scene.lookup("#righttab"))).getWidth());
+		
+		messageContents.setStyle("-fx-background-color: #202020;");
+		
+		// Text zum Content
+		
+		TextFlow textflow = new TextFlow();
+		
+		textflow.setPrefHeight(messageContents.getPrefHeight());
+		textflow.setPrefWidth(messageContents.getPrefWidth());
+		
+		textflow.setStyle("-fx-background-color: #281754");
+		
+		Text text = new Text(content);
+		text.setStyle("-fx-fill: #ffffff;");
+		
+		textflow.getChildren().add(text);
+		
+		messageContents.applyCss();
+		messageContents.layout();
+		
+		System.out.println(text.getHeight());
+		
+		messageContents.getChildren().add(textflow);
+		
+		messageContents.setPrefHeight( textflow.getPrefHeight() );
+		
+		MessagesBox.getChildren().add(messageContents);
+		
+		for(int i = 0; i < MessagesBox.getChildren().size(); ++i) {
+			
+			int result = 0;
+			
+			for(int j = i - 1; j >= 0; j--) {
+				result += ((Pane) MessagesBox.getChildren().get(j)).getPrefHeight();
+				
+				result += 5;
+			}
+			
+			((Pane) (MessagesBox.getChildren().get(i))).setLayoutY(result);
+			
+		}
+		
+		MessagesBox.setPrefHeight( ((Pane)MessagesBox.getChildren().get(MessagesBox.getChildren().size()-1)).getLayoutY() + ((Pane)MessagesBox.getChildren().get(MessagesBox.getChildren().size()-1)).getPrefHeight());
+		
+		ScrollPane ScrollPaneMSG = (ScrollPane) scene.lookup("#Scrl");
+		
+		ScrollPaneMSG.vvalueProperty().bind(MessagesBox.heightProperty());
 	}
 	
 	public static void main(String[] args) {
